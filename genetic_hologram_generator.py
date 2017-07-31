b03a6d56522198585238f229ca42f4d90f3b9836
Intradom <zboyang@caen-vnc-vm05.engin.umich.edu>
2017-07-31 16:58:03 -0400

    added some ifft stuff, moved code into functions

--- genetic_hologram_generator.py
+++ genetic_hologram_generator.py
@@ -1,5 +1,6 @@
 # This program uses a genetic algorithm to generate a hologram that matches an input image
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage.draw as skdraw
@@ -7,15 +8,20 @@ import skimage.draw as skdraw
 # Square pictures only
 IMAGE_SIDE_PIXELS = 480
 
-# Parameters
-MAX_SHAPE_SIZE = 50
+# Image constants
+MAX_INTENSITY = 255
 
-def evaluate_fitness(hologram):
-    recon_image = np.fft.ifft2(np.fft.fftshift(hologram))
-    plt.imshow(recon_image.real, cmap = 'gray')
-    plt.show()
-    
-    return 0
+# Parameters
+END_THRESHOLD = 0.1 # Percentage difference purely in difference of pixel intensity relative to max difference, lower fitness value is better
+MAX_SHAPE_SIZE = 50
+ADDITIONS_BEFORE_EVAL = 10
+
+def fitness(original, current):
+    # plt.imshow(current, cmap = 'gray', vmin = -255, vmax = 255) # Set vmin and vmax to force display not to automatically pick intensity range
+    # plt.show()
+
+    t = original - current
+    return np.sum(t) / (MAX_INTENSITY * 2 * np.square(IMAGE_SIDE_PIXELS))
 
 def draw_filled_circle(x, y, r):
     return skdraw.circle(int(x), int(y), int(r))
@@ -29,55 +35,67 @@ def draw_filled_rectangle(x, y, half_side):
 def draw_outline_rectangle(x, y, half_side):
     return skdraw.polygon_perimeter([x - half_side, x + half_side, x + half_side, x - half_side], [y - half_side, y - half_side, y + half_side, y + half_side])
 
+def add_hologram(holo_array):
+    # Create "blank canvas"        
+    holo_vals = np.empty(5)
+
+    # Determine which shape to add to canvas, and where to put it/size
+    holo_vals[0] = np.random.randint(4) # 0 to 3
+    holo_vals[1] = np.random.randint(MAX_SHAPE_SIZE, IMAGE_SIDE_PIXELS - MAX_SHAPE_SIZE)
+    holo_vals[2] = np.random.randint(MAX_SHAPE_SIZE, IMAGE_SIDE_PIXELS - MAX_SHAPE_SIZE)
+    holo_vals[3] = np.random.randint(MAX_SHAPE_SIZE) # Purposely allow for zero
+    holo_vals[4] = np.random.randint(-MAX_INTENSITY, MAX_INTENSITY + 1) # Max intensity is 255, random value from -255 to 255
+
+    holo_array.append(holo_vals)
+
+def eval_fit(original_image, holo_array)
+    cumulative_hologram = np.zeros([IMAGE_SIDE_PIXELS, IMAGE_SIDE_PIXELS])
+    for (i in range(len(holo_array))):
+        
+        current_holo_vals = holo_array[i]
+        temp_holo = np.zeros([IMAGE_SIDE_PIXELS, IMAGE_SIDE_PIXELS])
+        
+        if (current_holo_vals[0] == 0):
+            x, y = draw_filled_circle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
+        elif (current_holo_vals[0] == 1):
+            x, y = draw_outline_circle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
+        elif (current_holo_vals[0] == 2):
+            x, y = draw_filled_rectangle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
+        elif (current_holo_vals[0] == 3):
+            x, y = draw_outline_rectangle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
+
+        temp_holo[x, y] = current_holo_vals[4]
+        
+        cumulative_hologram += temp_holo
+    
+    # TESTING, show image of current cumulative hologram
+    # plt.imshow(np.fft.fftshift(cumulative_hologram), cmap = 'gray', vmin = -255, vmax = 255) # Set vmin and vmax to force display not to automatically pick intensity range
+    # plt.show()
+
+    # Evaluate fitness of current cumulative hologram
+    return fitness(original_image, np.fft.ifft2(cumulative_hologram)[:IMAGE_SIDE_PIXELS][:IMAGE_SIDE_PIXELS].real)
+
 def main():
     # Load target image
     
     # Create Hologram array to store individual shapes
     holo_array = []
 
+    best_fitness = (MAX_INTENSITY * 2 * np.square(IMAGE_SIDE_PIXELS)) # Max value fitness can take
     iter_counter = 0
     # Loop until fitness eval is within error threshold
-    while (True):
+    while (best_fitness > END_THRESHOLD):
         print("Iteration: %d"  % iter_counter)
     
-        # Create "blank canvas"        
-        holo_vals = np.empty(5)
+        for (i in range(ADDITIONS_BEFORE_EVAL)):
+            add_hologram(holo_array) # holo_array is modified in the function
+                
+        current_fitness = eval_fit(holo_array)
         
-        # Determine which shape to add to canvas, and where to put it/size
-        holo_vals[0] = np.random.randint(4) # 0 to 3
-        holo_vals[1] = np.random.randint(MAX_SHAPE_SIZE, IMAGE_SIDE_PIXELS - MAX_SHAPE_SIZE)
-        holo_vals[2] = np.random.randint(MAX_SHAPE_SIZE, IMAGE_SIDE_PIXELS - MAX_SHAPE_SIZE)
-        holo_vals[3] = np.random.randint(MAX_SHAPE_SIZE) # Purposely allow for zero
-        holo_vals[4] = np.random.randint(-255, 256) # Max intensity is 255, random value from -255 to 255
-                
-        holo_array.append(holo_vals)
-                
-        if (np.mod(iter_counter, 100) == 0):
-            cumulative_hologram = np.zeros([IMAGE_SIDE_PIXELS, IMAGE_SIDE_PIXELS])
-            for i in range(len(holo_array)):
-                
-                current_holo_vals = holo_array[i]
-                temp_holo = np.zeros([IMAGE_SIDE_PIXELS, IMAGE_SIDE_PIXELS])
-                
-                if (current_holo_vals[0] == 0):
-                    x, y = draw_filled_circle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
-                elif (current_holo_vals[0] == 1):
-                    x, y = draw_outline_circle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
-                elif (current_holo_vals[0] == 2):
-                    x, y = draw_filled_rectangle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
-                elif (current_holo_vals[0] == 3):
-                    x, y = draw_outline_rectangle(current_holo_vals[1], current_holo_vals[2], current_holo_vals[3])
-        
-                temp_holo[x, y] = current_holo_vals[4]
-                
-                cumulative_hologram += temp_holo
-            
-            # TESTING, show image of current cumulative hologram
-            plt.imshow(cumulative_hologram, cmap = 'gray', vmin = 0, vmax = 255) # Set vmin and vmax to force display not to automatically pick intensity range
-            plt.show()
-        
-            # Evaluate fitness of current cumulative hologram
-            evaluate_fitness(cumulative_hologram)
+        if (current_fitness < best_fitness):
+            best_fitness = current_fitness
+        else
+            holo_array = holo_array[:len(holo_array) - ADDITIONS_BEFORE_EVAL] # Remove the recently added-in hologram images
         
         iter_counter += 1
     
---
+++ hologram_test2.py
@@ -0,0 +1,79 @@
+from scipy import misc
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+from numpy.fft import fft2, ifft2, fftshift
+from numpy import zeros
+
+# Only square pictures
+if 0:
+  pic_name = str(sys.argv[1])
+  N = int(sys.argv[2]) # Specifies the side length of the square image to resize to
+
+N = 256
+
+if 1:
+  pic_name = "elephant.jpg"
+  A = misc.imread(pic_name, flatten=True)
+  A = misc.imresize(A, (N, N))
+
+if 0:
+  A = zeros((N,N),float)
+  A[(N>>1)-4:(N>>1)+4,(N>>1)-4:(N>>1)+4] = 1
+
+AA = zeros( (2*N,2*N), A.dtype )
+AA[:N,:N] = A
+AA[N:,:N] = A[::-1,:]
+AA[:N,N:] = A[:,::-1]
+AA[N:,N:] = A[::-1,::-1]
+
+FA = fft2(AA)
+DA = ifft2( FA.real )
+
+def lg(x):
+  return np.sign(x) * np.log(x)
+
+def limg(a):
+  return plt.imshow( lg(a), interpolation="nearest", cmap="gray")
+
+plt.figure(2)
+plt.subplot(121); plt.imshow(A)
+plt.subplot(122); plt.imshow(DA[:N,:N].real)
+
+plt.figure(1)
+plt.subplot(121); limg(fftshift(FA.real))
+plt.subplot(122); limg(fftshift(FA.imag))
+plt.show()
+"""
+raise "enough"
+
+
+A1 = np.fft.fftshift(np.fft.fft2(A))
+I1 = abs(A1)**2
+D2 = np.fft.ifft2(np.fft.fftshift(I1))
+
+plt.figure(1); clf()
+plt.subplot(2, 2, 1)
+plt.axis('off')
+plt.title('Original')
+plt.imshow(A, extent=[0, 1, 0, 1], cmap = 'gray')
+plt.subplot(2, 2, 2)
+plt.axis('off')
+plt.title('FFT')
+plt.imshow(I1, extent=[0, 1, 0, 1], cmap = 'gray')
+plt.subplot(2, 2, 3)
+plt.axis('off')
+plt.title('Reconstruction')
+plt.imshow(D2.real-A, extent=[0, 1, 0, 1], cmap = 'gray')
+colorbar()
+plt.show()
+
+l = arange(-64,64)
+x,y=meshgrid(l,l)
+r = asfarray(x*x+y*y)
+f = 1-r/(128.0+x*x*4+y*y)
+D3 = np.fft.ifft2(np.fft.fftshift(f*A1))
+figure(2); clf(); imshow(D2.real, extent=[0, 1, 0, 1], cmap = 'gray', interpolation='nearest'); colorbar()
+figure(3); clf(); imshow(D3.real-A,interpolation='nearest'); colorbar()
+"""
